ec59c6700cceed59cd5c618e54f10f7a06878aff
tr1vector <tr1vector@outlook.com>
2017-12-07 22:16:04 -0700

    Bloccit Assignment16 Seeding Data Complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,15 @@
- require 'random_data'
+require 'random_data'
 
- # Create Posts
- 50.times do
- # #1
-   Post.create!(
+# Create Posts
+50.times do
+# #1
+Post.create!(
  # #2
-     title:  RandomData.random_sentence,
-     body:   RandomData.random_paragraph
-   )
- end
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph
+    )
+end
+
  posts = Post.all
  
  # Create Comments
@@ -20,6 +21,11 @@
      body: RandomData.random_paragraph
    )
  end
+
+Post.find_or_create_by( title: "The Last of Us 2",
+  body: "Prepare for the biggest game of 2018 with The Last of Us 2!")
+
+Comment.find_or_create_by( post: Post.find(51), body: 'First Comment about the Last of Us 2!')
  
  puts "Seed finished"
  puts "#{Post.count} posts created"
