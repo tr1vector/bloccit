80147e81c0b638357c5eab6090acbc565843d14f
tr1vector <tr1vector@outlook.com>
2017-12-08 23:43:06 -0700

    Bloccit Assignment 18 CRUD complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,18 +17,13 @@ class PostsController < ApplicationController
   end
 
   def create
- # #9
     @post = Post.new
     @post.title = params[:post][:title]
     @post.body = params[:post][:body]
-
- # #10
     if @post.save
- # #11
       flash[:notice] = "Post was saved."
       redirect_to @post
     else
- # #12
       flash.now[:alert] = "There was an error saving the post. Please try again."
       render :new
     end
